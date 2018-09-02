dbbb67853996ccf0a82030b1a042badeb8b5e2b2
Thomas Kay <tkay@medprocure.com>
2018-09-02 10:47:07 -0400

    restructured file organization

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,18 +1,14 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import About from '../components/About';
-import NotFoundPage from '../components/NotFoundPage';
-import Home from '../pages/Home';
-import Welcome from '../pages/Welcome';
+import { Home } from '../modules/home';
+import { NotFoundPage } from '../modules/not-found-page';
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Switch>
         <Route exact={true} path="/" component={Home} />
-        <Route path="/welcome" component={Welcome} />
-        <Route path="/about" component={About} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>

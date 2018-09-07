05f0a6c8f8652ab8f17c7696ee6fbc877142d3e9
Thomas Kay <tkay@medprocure.com>
2018-09-06 23:23:24 -0400

    wip connecting typeorm

--- src/server.ts
+++ src/server.ts
@@ -2,37 +2,52 @@ import * as bodyParser from "body-parser";
 import * as express from 'express';
 import { Request, Response } from "express";
 
-// create and setup express app
-const app = express();
-app.use(bodyParser.json());
+import { createConnection } from "net";
+import { User } from "./entities/User";
 
-// register routes
-app.get("/users", (req: Request, res: Response) => {
-  // here we will have logic to return all users
+import { dbOptions } from '../app-config';
+
+createConnection(dbOptions).then((connection: any) => {
+  const userRepository = connection.getRepository(User);
+
+  // create and setup express app
+  const app = express();
+  app.use(bodyParser.json());
+
+  // register routes
+  app.get("/users", async (req: Request, res: Response) => {
+    // here we will have logic to return all users
+    return userRepository.find();
+  });
+
+  app.get("/users/:id", (req: Request, res: Response) => {
+    // here we will have logic to return user by id
+    return userRepository.findOne(req.params.id);
+  });
+
+  app.post("/users", (req: Request, res: Response) => {
+    // here we will have logic to save a user
+    const user = userRepository.create(req.body);
+    return userRepository.save(user);
+  });
+
+  // app.put("/users/:id", (req: Request, res: Response) => {
+  //   // here we will have logic to update a user by a given user id
+  // });
+
+  app.delete("/users/:id", (req: Request, res: Response) => {
+    // here we will have logic to delete a user by a given user id
+    return userRepository.remove(req.params.id);
+  });
+
+
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Awesome! We're live debugging this!");
+  });
+
+  app.listen(8000, () => console.log("Server running on 8000!"));
+
+
+
 });
-
-app.get("/users/:id", (req: Request, res: Response) => {
-  // here we will have logic to return user by id
-});
-
-app.post("/users", (req: Request, res: Response) => {
-  // here we will have logic to save a user
-});
-
-app.put("/users/:id", (req: Request, res: Response) => {
-  // here we will have logic to update a user by a given user id
-});
-
-app.delete("/users/:id", (req: Request, res: Response) => {
-  // here we will have logic to delete a user by a given user id
-});
-
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Awesome! We're live debugging this!");
-});
-
-app.listen(8000, () => console.log("Server running on 8000!"));
-
-
 

7067d819f451df32dacd6c7ca3743195d513914b
Thomas Kay <tkay@medprocure.com>
2018-09-06 22:21:34 -0400

    added basic express handlers; setting up typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,38 @@
+import * as bodyParser from "body-parser";
 import * as express from 'express';
+import { Request, Response } from "express";
 
+// create and setup express app
 const app = express();
+app.use(bodyParser.json());
 
-app.listen(8000, () => console.log("Server running on 8000!"));
+// register routes
+app.get("/users", (req: Request, res: Response) => {
+  // here we will have logic to return all users
+});
 
-app.get("/", (req, res) => {
+app.get("/users/:id", (req: Request, res: Response) => {
+  // here we will have logic to return user by id
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  // here we will have logic to save a user
+});
+
+app.put("/users/:id", (req: Request, res: Response) => {
+  // here we will have logic to update a user by a given user id
+});
+
+app.delete("/users/:id", (req: Request, res: Response) => {
+  // here we will have logic to delete a user by a given user id
+});
+
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Awesome! We're live debugging this!");
 });
 
+app.listen(8000, () => console.log("Server running on 8000!"));
 
 
 

e7270a63e192a4599cd3a157288d5daf7a3fcbce
Thomas Kay <tkay@medprocure.com>
2018-09-15 17:30:01 -0400

    wip sign in modal

--- src/modules/components/welcome/Welcome.tsx
+++ src/modules/components/welcome/Welcome.tsx
@@ -1,19 +1,40 @@
 import * as React from "react";
 import { Link } from 'react-router-dom';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 
 import * as charSkyline from "@media/charlotteSkyline.jpg";
 import * as charSkyView from "@media/charlotteSkyView.mp4";
 import * as charSkyViewWebm from "@media/charlotteSkyView.webm";
 
-import { Button, Typography } from '@material-ui/core';
+import { Button, Slide, Typography, Fade } from '@material-ui/core';
+
+import { SignIn } from '@components/signin';
 
 /**
  * Render the landing page and provide a button to navigate to the app.
  */
+@observer
 export class Welcome extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {};
+  @observable showSignIn: boolean = false;
+  private greetingRef = React.createRef<HTMLDivElement>();
+
+  async componentDidMount() {
+
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(true);
+      }, 5.3e3);
+    });
+
+    promise.then(() => {
+      document.getElementById('greeting')!.style.display = 'none';
+      this.showSignIn = true;
+    });
+  }
+
+  public handleSignIn = () => {
+    // handlesignin
   }
 
   public render() {
@@ -29,26 +50,13 @@ export class Welcome extends React.Component<any, any> {
             />
           </video>
         </div>
-        {this.renderWelcome()}
+        <section className="welcome">
+          <Typography id="greeting" className="welcome__greeting slide-in-bck-center" variant="display4" align="center">Welcome to Charlotte</Typography>
+          <Slide direction="up" in={this.showSignIn} mountOnEnter unmountOnExit timeout={1.1e3}>
+            <SignIn handleSignIn={this.handleSignIn} />
+          </Slide>
+        </section>
       </header>
     );
   }
-
-  public renderWelcome = () => (
-    <section className="welcome">
-      <Typography variant="display4" align="center">Welcome to Charlotte</Typography>
-      <div className="welcome__button-container">
-        <Link to="/favorite">
-          <Button
-            className="hero-enter-btn"
-            variant="contained"
-            color="primary"
-            size="large"
-          >
-            Enter
-        </Button>
-        </Link>
-      </div>
-    </section>
-  )
 }
